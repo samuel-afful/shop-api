232b120af3bd6307396066daaa88c02cf1b48733
unknown <saffulkoranteng@gmail.com>
2022-07-17 10:39:19 -0700

    Verify Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require('crypto-js')
-
 var jwt = require('jsonwebtoken');
 
 //REGISTER
@@ -29,14 +28,17 @@ router.post('/login',async(req,res)=>{
     //takes the user password from database from matched username
     //Decrypting password and matching with plain password input
     const hashedPassword  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-    if (password !== req.body.password ) return res.status(201).json("Wrong Credentials");   
-       const token = jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.TOKEN_KEY);
-      res.status(200).json({user,token});
+    const originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    if (originalpassword !== req.body.password ) return res.status(201).json("Wrong Credentials");   
+       const token = jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.TOKEN_KEY,{expiresIn:"3d"});
+       const {password, ...others} = user
+      res.status(200).json({others,token});
 }catch(err){
       res.status(501).json(err);
 }
 });
+
+
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,32 @@
-
+const User = require("../models/User");
+const {verifyTokenAndAuthorization} = require("../routes/verifywebtoken")
 const router = require("express").Router();
+const CryptoJS = require('crypto-js');
 
+
+router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
+   if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(req.body.password,process.env.SECRET_KEY).toString()
+   }
+   try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{new:true})
+        res.status(200).json(updatedUser)
+   }catch(err){
+    res.status(500).json(err)
+   }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async(req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Deleted")
+    }catch(err){
+        res.status(500).json(err)
+    }
+  
+});
 
 
 
