3066861f49629c8787474ed87ad705341a81ada2
unknown <saffulkoranteng@gmail.com>
2022-10-14 07:28:51 -0700

    ADDED: compression and Helmet

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 const express = require("express");
-const app = express()
+const app = express();
 const mongoose = require("mongoose");
-const testRouter = require("./routes/stripe");
+var compression = require('compression');
+var helmet = require("helmet");
+
 const cartRouter = require("./routes/cart");
 const orderRouter = require("./routes/order");
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const prodRouter = require('./routes/product');
-const stripeRouter = require("./routes/stripe")
+const stripeRouter = require("./routes/stripe");
 const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(express.json())
-
+app.use(express.json());
+app.use(compression());
+app.use(helmet());
 
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-console.log("DB connected")
+console.log("DB connected");
 }).catch((err)=>{
-    console.log(err)
+    console.log(err);
 });
 
 //Routing
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/product',prodRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-app.use('/api/stripe',stripeRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+app.use('/api/product',prodRouter);
+app.use('/api/cart',cartRouter);
+app.use('/api/order',orderRouter);
+app.use('/api/stripe',stripeRouter);
 
-port = process.env.PORT || 5000
+port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
-    console.log(`Listen on port ${port}`)
-})
+    console.log(`Listen on port ${port}`);
+});
