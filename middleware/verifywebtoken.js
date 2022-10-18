a583f1778cb49ef757483217efa30d3f85998eb7
unknown <saffulkoranteng@gmail.com>
2022-10-18 03:31:04 -0700

    FIXED: Token issues

--- middleware/verifywebtoken.js
+++ middleware/verifywebtoken.js
@@ -4,7 +4,7 @@ const verifyToken = function(req,res,next){
 const token = req.header('x-auth-token')
 if(!token) return res.status(401).send("Access Denied, No Token Provided");
 try{
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const decoded = jwt.verify(token, config.get("TOKEN_KEY"));
     req.user = decoded ;
     next();
 }  
