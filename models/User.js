46ccfca3139b2d1b0ab04ed1d1eedea72b11cf76
unknown <saffulkoranteng@gmail.com>
2022-10-18 05:31:36 -0700

    FIXED: Token issues

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 var jwt = require('jsonwebtoken');
-
+const config = require("config")
 const UserSchema = new mongoose.Schema({
     username:{type:String, required:true, unique:true},
     email:{type:String, required:true, unique:true},
